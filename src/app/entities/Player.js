92bbed6461d4cc6dbf0158a30453394f987ed375
Daniel Adams <msub2official@gmail.com>
2021-08-15 22:39:58 -0400

    Use working commit, add controller+hand tracking

--- src/app/entities/Player.js
+++ src/app/entities/Player.js
@@ -1,26 +1,18 @@
 export default class Player {
     constructor() {
         this.scene = document.querySelector('a-scene');
-        
+
         this.leftHand = document.createElement('a-entity');
-        this.leftHand.setAttribute('hand-controls', {
-            hand: 'left',
-            handModelStyle: 'lowPoly',
-        });
-        this.leftHand.setAttribute('hand-tracking-controls', {
-            hand: 'left'
-        });
-
         this.rightHand = document.createElement('a-entity');
-        this.rightHand.setAttribute('hand-controls', {
-            hand: 'right',
-            handModelStyle: 'lowPoly',
-        });
-        this.rightHand.setAttribute('hand-tracking-controls', {
-            hand: 'right'
-        });
 
-        this.scene.appendChild(this.leftHand);
-        this.scene.appendChild(this.rightHand);
+        [this.leftHand, this.rightHand].forEach((hand, i) => {
+            var handedness = i == 0 ? 'left' : 'right'
+            hand.setAttribute('oculus-touch-controls', { hand: handedness });
+            hand.setAttribute('vive-controls', { hand: handedness });
+            hand.setAttribute('windows-motion-controls', { hand: handedness });
+            hand.setAttribute('daydream-controls', { hand: handedness });
+            hand.setAttribute('hand-tracking-controls', { hand: handedness });
+            this.scene.appendChild(hand);
+        });
     }
 }
